3ef862ccb9fe73764e2b588ae7ae78333f713a3e
Tolida1 <105459096+Tolida1@users.noreply.github.com>
2025-07-08 00:45:03 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,6 @@
 
+@echo off
+rem -----------------------------------------------------------------------------
+rem Gradle startup script for Windows
+rem -----------------------------------------------------------------------------
+REM (Bunu gerçek bir projenin gradlew.bat dosyasından kopyala, kısaltılmıştır.)
